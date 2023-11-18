7e8c21c6ca0a5f06f2e871bd77611921365c84a8
Rana Mahdavi <rana.mahdavi@randstad.com>
2023-11-18 18:16:53 +0100

    updated

--- adress_to_country/business_logic.py
+++ adress_to_country/business_logic.py
@@ -9,33 +9,35 @@ class AddressToCountryConverter:
         self.city_to_country = address_data_access.load_cities()
         self.addresses_data = addresses
         self.country_names_to_code = address_data_access.create_country_mapping()
-        self.multiword_cities = {city for city, item in self.city_to_country.items() if ' ' in item}
-
+        self.multiword_cities = set(city for city in self.city_to_country if ' ' in city)
     def find_country_in_address(self, address):
         normalized_address = self.address_data_access.normalize(address)
 
         for multiword_city in self.multiword_cities:
             if multiword_city in normalized_address:
-                return next((item.get('country', "Unknown") for item in self.city_to_country if
-                             item.get('city') == multiword_city), "Ujnknown")
+                return self.city_to_country.get(multiword_city, "Unknown")
 
         tokens = normalized_address.split()
         for token in tokens:
             if token in self.country_names_to_code:
                 return self.country_names_to_code[token]
-            elif any(Levenshtein.distance(token, city) <= 2 for city in self.multiword_cities):
-                return next(
-                    country for city, country in self.city_to_country.items() if Levenshtein.distance(token, city) <= 2
-                )
+            elif token in self.city_to_country:
+                return self.city_to_country[token]
+
+            for city in self.city_to_country.keys():
+                if Levenshtein.distance(token, city) <= 2:
+                    return self.city_to_country[city]
 
         return "Unknown"
     def process_addresses(self):
         results = {}
         for address_dict in self.addresses_data:
-            address_str = address_dict.get('address', '')
+            address_str = address_dict['address']
             country = self.find_country_in_address(address_str)
             results[address_str] = country
         return results
+
+    @staticmethod
     def get_hash_of_results(results):
         results_str = json.dumps(results, sort_keys=True)
         return hashlib.sha256(results_str.encode()).hexdigest()
--- adress_to_country/test.py
+++ adress_to_country/test.py
@@ -8,11 +8,17 @@ addresses_path = 'dataset/addresses.jsonl'
 
 address_data_access = AddressDataAccess(cities_path, addresses_path)
 all_addresses = address_data_access.load_addresses()
-subset_addresses = random.sample(all_addresses, 100)
+sample_n=100
+subset_addresses = random.sample(all_addresses, sample_n)
 converter = AddressToCountryConverter(address_data_access, subset_addresses)
+
 result = converter.process_addresses()
+known_count = 0
 
 for address, country in result.items():
     result_str = f"Address: {address} >> Country: {country}"
     result_hash = hashlib.sha256(result_str.encode()).hexdigest()
     print(f"{result_str} >> Hash: {result_hash}")
+    if country != "Unknown":
+        known_count += 1
+print(f"Number of addresses with detected country: {known_count} out of 100 samples")
